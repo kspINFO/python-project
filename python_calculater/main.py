56349eec0910d51cab87a646e257a084405158e5
kspINFO <pugalk933@gmail.com>
2025-05-15 13:54:32 +0530

    calculator is complite

--- python_calculater/main.py
+++ python_calculater/main.py
@@ -1,22 +1,22 @@
 # python calculator
 
+while True:
 
+    opurator=input("Enter an operator(+,-,*,/):")
+    num1=float(input("Enter the 1st number: "))
+    num2=float(input("Emter the 2nd number: "))
 
-operator=input("Enter an operator(+,-,*,/):")
-num1=float(input("Enter the 1st number: "))
-num2=float(input("Emter the 2nd number: "))
-
-if operator=="+":
-    result=num1+num2
-    print(result)
-elif operator=='-':
-    result=num1-num2
-    print(result)
-elif operator=='*':
-    result=num1*num2 
-    print(result)
-elif operator=='/':
-    result=num1/num2
-    print(result)
-else:
-    print(f"{operator} this invalied")
+    if opurator=="+":
+        result=num1+num2
+        print(round(result,3))
+    elif opurator=='-':
+        result=num1-num2
+        print(round(result,3))
+    elif opurator=='*':
+        result=num1*num2 
+        print(round(result,3))
+    elif opurator=='/':
+        result=num1/num2
+        print(round(result,3))
+    else:
+        print(f"{opurator} this invalied opurator")
